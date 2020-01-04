028f9f1d740cebcc25f851bc5f27c5f9d7d59b77
josephsy96 <josephsy96@hotmail.com>
2020-01-03 19:43:35 -0600

    fixed query

--- app.py
+++ app.py
@@ -1,2 +1,38 @@
 #import flask dependency
-from flask import Flask
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+
+#create sql engine to connect to
+engine = create_engine('sqlite:///Resources/hawaii.sqlite')
+
+#creates a reflection for tables
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    print("Welcome to my weather API!")
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route('/api/v1.0/precipitation')
+def precip():
+    print('Precipitation Page')
+    prcp = 
+    
